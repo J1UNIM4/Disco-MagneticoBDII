19dd9653b059a3d30847640f7ad3227cb03a15d7
J1UNIM4 <jossein.morales@ucsp.edu.pe>
2024-11-26 10:30:34 -0500

    avnze

--- disco.js
+++ disco.js
@@ -1,6 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('myButton');
-    button.addEventListener('click', () => {
-        alert('hola mundo');
-    });
+const butonS = document.getElementById("searchB");
+
+let searchv;
+butonS.addEventListener('click',()=>{
+    searchv = document.getElementById('searchT').value;
+    alert(searchv);
+    console.log(searchv);
+});
+
+let discomagnetico = [[[[]]],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]];
+
+let t;
+let b;
+let v;
+const butonI = document.getElementById("insertionB");
+butonI.addEventListener('click',()=>{
+    t = document.getElementById('type');
+    b = document.getElementById('bytes');
+    v = document.getElementById('value');
+
+    discomagnetico[0][0][0].push(t.value,b.value,v.value);
+    console.log(discomagnetico[0][0][0]);
+    t.value = ' ';
+    b.value = ' ';
+    v.value = ' ';
 });
